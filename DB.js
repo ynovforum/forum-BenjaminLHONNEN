0225f80260489a0279379d37185d4483ae81e284
BenjaminLHONNEN <benjaminlhonnen@gmail.com>
2018-05-28 09:42:12 +0200

    load empty template

--- DB.js
+++ DB.js
@@ -2,7 +2,7 @@ const mysql2 = require('mysql2');
 const Sequelize = require('sequelize');
 
 
-const db = new Sequelize('youtube', 'root', '', {
+const db = new Sequelize('forum', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
@@ -13,26 +13,7 @@ const User = db.define('user', {
     userName: {type: Sequelize.STRING},
 });
 
-const Follows = db.define('follow', {
-    userId: {type: Sequelize.INTEGER},
-    channelId: {type: Sequelize.STRING},
-});
-
-const Comment = db.define('comment', {
-    idVideo: { type: Sequelize.STRING },
-    idUser: { type: Sequelize.INTEGER },
-    comment: { type: Sequelize.TEXT },
-});
-
 User.hasMany(Follows, {foreignKey: 'userId'});
-Follows.belongsTo(User, {foreignKey: 'userId'});
-
-User.hasMany(Comment, { foreignKey: 'idUser' });
-Comment.belongsTo(User, { foreignKey: 'idUser' });
-
-User.sync();
-Comment.sync();
-Follows.sync();
 
 module.exports = {
     User: User,
--- const.js
+++ const.js
@@ -1,9 +1,7 @@
 
 const COOKIE_SECRET = 'le mage noir';
-const YOUTUBE_API_KEY = 'AIzaSyAFztrzYVEzqtdBdm2fqgka9CGLcF0Gh_w';
 
 
 module.exports = {
     COOKIE_SECRET: COOKIE_SECRET,
-    YOUTUBE_API_KEY: YOUTUBE_API_KEY,
 };
